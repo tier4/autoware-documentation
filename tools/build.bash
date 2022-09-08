88ddbef2a4cc1a7d2ac682bcfff0330a85ea0fce
Takagi, Isamu <isamu.takagi@tier4.jp>
2022-09-08 09:16:58 +0900

    feat: add script
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

---
+++ tools/build.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+colcon build --symlink-install --packages-up-to autoware_interface_document
---
+++ tools/generate.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+source install/setup.bash
+ros2 run autoware_interface_document generate
+find docs/design/autoware-interfaces/ad-api | xargs pre-commit run --files
